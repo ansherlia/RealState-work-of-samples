a69d3f3195ee808d008bede093a1c3aab1d48bc2
reza hagju <ansherlireza2020@gmail.com>
2025-04-07 14:12:24 +0330

    fixed a bug

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,8 +2,10 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import DashboardPage from "@/components/templates/DashboardPage";
+import connectDB from "@/utils/connectDB";
 
 async function Dashboard() {
+  await connectDB()
   const session = await getServerSession(authOptions);
   if (!session) redirect("/signup");
   return <DashboardPage email={session.user.email} />;
