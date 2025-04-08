32066a82b1a40499e9c155c799b04d0382895a87
reza hagju <ansherlireza2020@gmail.com>
2025-04-08 14:58:32 +0330

    some bugs resolved

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -80,7 +80,7 @@ function Header() {
                   </li>
                   <li>
                     {" "}
-                    <Link href="/buy-residential" className="">
+                    <Link href="/buy-residentials" className="">
                       آگهی ها
                     </Link>
                   </li>
